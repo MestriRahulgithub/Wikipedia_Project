3c8bad7ac4250d49af57c7fb0ca6c1d1ecea2827
MestriRahulgithub <118504920+MestriRahulgithub@users.noreply.github.com>
2023-09-03 13:32:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+function addTask() {
+    var taskInput = document.getElementById("taskInput");
+    var taskList = document.getElementById("taskList");
+    var taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task.");
+        return;
+    }
+
+    var listItem = document.createElement("li");
+    listItem.textContent = taskText;
+    taskList.appendChild(listItem);
+
+    taskInput.value = "";
+}
